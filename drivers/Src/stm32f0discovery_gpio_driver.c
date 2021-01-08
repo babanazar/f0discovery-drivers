e62f307f6b6b1836b4ce17ca403f292a4c186f8a
Babanazar Gutlygeldiyev <g.babanazar@gmail.com>
2021-01-08 12:57:41 +0300

    Interrupt interface is complete

--- drivers/Src/stm32f0discovery_gpio_driver.c
+++ drivers/Src/stm32f0discovery_gpio_driver.c
@@ -240,10 +240,15 @@ void GPIO_IRQPriorityConfig(uint8_t IRQNumber, uint8_t IRQPriority)
 	uint8_t iprx_section = IRQNumber % 4;
 
 	uint8_t shift_amount = ( 8 * iprx_section ) + (8 - NO_PR_BITS_IMPLEMENTED);
-	*(NVIC_PR_BASE_ADDR + iprx * 4) |= ( IRQPriority << ( 8 * iprx_section) );
+	*(NVIC_PR_BASE_ADDR + iprx * 4) |= ( IRQPriority << shift_amount );
 }
 
 void GPIO_IRQHandling(uint8_t PinNumber)
 {
-
+	/* Clear the exti pr register corresponding to the pin number */
+	if(EXTI->PR & (1 << PinNumber))
+	{
+		/* Clear */
+		EXTI->PR |= (1 << PinNumber);
+	}
 }
